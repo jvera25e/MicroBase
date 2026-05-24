12b05f3cf76d850f12254bce1ef8a72dfab0afb7
Johao Castillo <wjcm.0604952291@gmail.com>
2026-05-24 11:13:57 -0500

    Eliminar scripts temporales de prueba

--- fetch.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import urllib.error
-try:
-    res = urllib.request.urlopen('http://127.0.0.1:8000/staff')
-    print("SUCCESS", res.getcode(), res.geturl())
-except urllib.error.HTTPError as e:
-    print("ERROR", e.code, e.read().decode())
